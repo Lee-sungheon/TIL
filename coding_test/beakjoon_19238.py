42aeae35c0b4ae8dbe78630d089cc41ef64ba67d
Lee-sungheon <leesungheon.dev@gmail.com>
2020-09-17 15:15:43 +0900

    200917 TIL

--- coding_test/beakjoon_19238.py
+++ coding_test/beakjoon_19238.py
@@ -1,3 +1,4 @@
+import copy
 # 스타트 택시
 def bfs(start, fuel):
     dq = [(start, 0, fuel)]
@@ -12,10 +13,10 @@ def bfs(start, fuel):
         f = tmp[2]
         if len(end) != 0 and cnt == end[-1][1]+1:
             return end
-        if f <= 0:
-            return end
-        elif matrix[x][y] > 1:
+        if matrix[x][y] > 1:
             end.append(([x, y], cnt))
+        if f < 0:
+            return end
         for dx, dy in delta:
             nx, ny = x + dx, y + dy
             if nx < 0 or ny < 0 or nx >= N or ny >= N:
@@ -23,9 +24,11 @@ def bfs(start, fuel):
             if matrix[nx][ny] != 1 and visited[nx][ny] == 0:
                 visited[nx][ny] = 1
                 dq.append(([nx, ny], cnt+1, f-1))
+    if len(end) != 0:
+        return end
 
 
-def bfs2(start, fuel, man_number):
+def bfs2(start, fuel):
     dq = [(start, 0, fuel)]
     delta = [(0, 1), (1, 0), (-1, 0), (0, -1)]
     visited = [[0]*N for _ in range(N)]
@@ -36,38 +39,33 @@ def bfs2(start, fuel, man_number):
         x, y = tmp[0]
         cnt = tmp[1]
         f = tmp[2]
-        if matrix[x][y] == -man_number:
+        if matrix2[x][y] == -1:
             end.append(([x, y], cnt))
+            matrix2[x][y] = 0
             return end
-        for sam in same:
-            same_tmp = sam
-            if x == same_tmp[0][0] and y == same_tmp[0][1] and same_tmp[1] == -man_number:
-                end.append(([same_tmp[0][0], same_tmp[0][1]], cnt))
-                return end
         if f <= 0:
             return end
         for dx, dy in delta:
             nx, ny = x + dx, y + dy
             if nx < 0 or ny < 0 or nx >= N or ny >= N:
                 continue
-            if matrix[nx][ny] != 1 and visited[nx][ny] == 0:
+            if matrix2[nx][ny] != 1 and visited[nx][ny] == 0:
                 visited[nx][ny] = 1
                 dq.append(([nx, ny], cnt+1, f-1))
+    if len(end) != 0:
+        return end
 
 
 N, M, fuel = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(N)]
+matrix2 = copy.deepcopy(matrix)
 start = list(map(int, input().split()))
 start[0], start[1] = start[0] - 1, start[1] - 1
-same = []
+des = []
 for i in range(M):
     tmp = list(map(int, input().split()))
     matrix[tmp[0]-1][tmp[1]-1] = i + 2
-    if matrix[tmp[2]-1][tmp[3]-1] != 0:
-        same.append(((tmp[0]-1, tmp[1]-1), -(i + 2)))
-    else:
-        matrix[tmp[2]-1][tmp[3]-1] = - (i + 2)
-man_number = 0
+    des.append(((tmp[2]-1, tmp[3]-1), (i + 2)))
 for i in range(M):
     temp = bfs(start, fuel)
     if not temp:
@@ -78,10 +76,11 @@ for i in range(M):
     result = temp[0]
     fuel -= result[1]
     start = result[0]
-    man_number = matrix[start[0]][start[1]]
+    for d in des:
+        if d[1] == matrix[start[0]][start[1]]:
+            matrix2[d[0][0]][d[0][1]] = -1
     matrix[start[0]][start[1]] = 0
-
-    temp = bfs2(start, fuel, man_number)
+    temp = bfs2(start, fuel)
     if not temp:
         print(-1)
         break
