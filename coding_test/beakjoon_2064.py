ec7df86cde93b1065fea044f287a5f3a56ae418d
Lee-sungheon <leesungheon.dev@gmail.com>
2020-09-18 11:12:01 +0900

    200918 TIL

--- coding_test/beakjoon_2064.py
+++ coding_test/beakjoon_2064.py
@@ -1,14 +1,14 @@
 # IP 주소
-N = int(input())
-ip_lists = [list(map(int, input().split('.'))) for _ in range(N)]
+import sys
+
+N = int(sys.stdin.readline())
+ip_lists = [list(map(int, sys.stdin.readline().split('.'))) for _ in range(N)]
 cnt_lists = [0]*4
 address = ['0b']*4
 mask = ['0b']*4
-
 for i in range(N):
     for j in range(4):
         ip_lists[i][j] = '0b' + format(ip_lists[i][j], 'b').zfill(8)
-ip_lists.sort()
 for i in range(4):
     for j in range(2, 10):
         cnt = []
@@ -21,24 +21,13 @@ for i in range(4):
 for i in range(1, 4):
     if cnt_lists[i-1] != 8:
         cnt_lists[i] = 0
-
 for i in range(4):
     for j in range(cnt_lists[i]):
         address[i] += ip_lists[0][i][j+2]
-    for j in range(cnt_lists[i], 8):
-        address[i] += '0'
-
-for i in range(4):
-    for _ in range(cnt_lists[i]):
         mask[i] += '1'
+    for _ in range(cnt_lists[i], 8):
+        address[i] += '0'
     for _ in range(8-cnt_lists[i]):
         mask[i] += '0'
-
-for i in range(N):
-    print(ip_lists[i])
-print(cnt_lists)
-print(address)
-print(mask)
-
 print(f'{int(address[0], 2)}.{int(address[1], 2)}.{int(address[2], 2)}.{int(address[3], 2)}')
 print(f'{int(mask[0], 2)}.{int(mask[1], 2)}.{int(mask[2], 2)}.{int(mask[3], 2)}')
---
+++ git_branch.md
@@ -0,0 +1,31 @@
+# branch
+> git branch 명령어 정리
+
+## 개념
+- 나무가지라는 의미로 코드의 분기를 의미함
+
+## 확인
+ - `git branch`
+
+ ## 생성
+ - `git branch <new branch name>`
+
+ ## 이동
+ - `git switch <branch>`
+
+ ## 삭제
+ - `git branch -d <branch name>`
+
+## 합병
+- `git merge <branch name>`
+
+
+```python
+def login():
+  pass
+```
+ 
+```python
+def logout():
+  pass
+```
