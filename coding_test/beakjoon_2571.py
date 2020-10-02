a953602b46a3878a979c367d21bd6610ca40aeec
Lee-sungheon <leesungheon.dev@gmail.com>
2020-10-02 15:43:25 +0900

    201002 practice codint test

--- coding_test/beakjoon_2571.py
+++ coding_test/beakjoon_2571.py
@@ -1,4 +1,22 @@
 # 색종이 - 3
+def find_index(i, j):
+    col_min.append(j)
+    row_min.append(i)
+    for m in range(i, 101):
+        if matrix[m][j] > 1 and (m == 100 or matrix[m+1][j] <= 1):
+            row_max.append(m)
+            break
+    for n in range(j, 101):
+        if matrix[i][n] > 1 and (n == 100 or matrix[i][n+1] <= 1):
+            col_max.append(n)
+            break
+
+    for i in range(row_min[-1], row_max[-1] + 1):
+        for j in range(col_min[-1], col_max[-1] + 1):
+            matrix[i][j] = 110
+    return
+
+
 N = int(input())
 matrix = [[0]*101 for _ in range(101)]
 for _ in range(N):
@@ -6,74 +24,67 @@ for _ in range(N):
     for i in range(y, y+10):
         for j in range(x, x + 10):
             matrix[i][j] += 1
-# for i in range(101):
-#     print(matrix[i])
 
-col_max = row_max = 0
-col_min = row_min = 101
+col_max, row_max = [], []
+col_min, row_min = [], []
 for i in range(101):
     for j in range(101):
-        if matrix[i][j] > 1:
-            if j > col_max:
-                col_max = j
-            if i > row_max:
-                row_max = i
-            if j < col_min:
-                col_min = j
-            if i < row_min:
-                row_min = i
-if col_min == 101:
-    col_min = 0
-if row_min == 101:
-    row_min = 0
-col_cnt = row_max - row_min
-for i in range(row_min-1, -1, -1):
-    tmp = 0
-    for j in range(col_min, col_max+1):
-        if matrix[i][j] >= 1:
-            tmp += 1
+        if 101 > matrix[i][j] > 1:
+            find_index(i, j)
+
+max_result = 0
+for k in range(len(row_min)):
+    if col_min[k] == 101:
+        col_min[k] = 0
+    if row_min[k] == 101:
+        row_min[k] = 0
+    col_cnt = row_max[k] - row_min[k]
+    for i in range(row_min[k] - 1, -1, -1):
+        tmp = 0
+        for j in range(col_min[k], col_max[k] + 1):
+            if matrix[i][j] >= 1:
+                tmp += 1
+            else:
+                break
+        if tmp == col_max[k] - col_min[k] + 1:
+            col_cnt += 1
         else:
             break
-    if tmp == col_max - col_min + 1:
-        col_cnt += 1
-    else:
-        break
-for i in range(row_max, 101):
-    tmp = 0
-    for j in range(col_min, col_max+1):
-        if matrix[i][j] >= 1:
-            tmp += 1
+    for i in range(row_max[k], 101):
+        tmp = 0
+        for j in range(col_min[k], col_max[k] + 1):
+            if matrix[i][j] >= 1:
+                tmp += 1
+            else:
+                break
+        if tmp == col_max[k] - col_min[k] + 1:
+            col_cnt += 1
         else:
             break
-    if tmp == col_max - col_min + 1:
-        col_cnt += 1
-    else:
-        break
-row_cnt = col_max - col_min
-for i in range(col_min-1, -1, -1):
-    tmp = 0
-    for j in range(row_min, row_max+1):
-        if matrix[j][i] >= 1:
-            tmp += 1
+
+    row_cnt = col_max[k] - col_min[k]
+    for i in range(col_min[k] - 1, -1, -1):
+        tmp = 0
+        for j in range(row_min[k], row_max[k] + 1):
+            if matrix[j][i] >= 1:
+                tmp += 1
+            else:
+                break
+        if tmp == row_max[k] - row_min[k] + 1:
+            row_cnt += 1
         else:
             break
-    if tmp == row_max - row_min + 1:
-        row_cnt += 1
-    else:
-        break
-for i in range(col_max, 101):
-    tmp = 0
-    for j in range(row_min, row_max+1):
-        if matrix[j][i] >= 1:
-            tmp += 1
+    for i in range(col_max[k], 101):
+        tmp = 0
+        for j in range(row_min[k], row_max[k] + 1):
+            if matrix[j][i] >= 1:
+                tmp += 1
+            else:
+                break
+        if tmp == row_max[k] - row_min[k] + 1:
+            row_cnt += 1
         else:
             break
-    if tmp == row_max - row_min + 1:
-        row_cnt += 1
-    else:
-        break
-col_max, row_max = col_max + 1, row_max + 1
-print(col_min, col_max, row_min, row_max)
-print(col_cnt * (col_max - col_min))
-print(row_cnt * (row_max - row_min))
-print(max(col_cnt * (col_max - col_min), row_cnt * (row_max - row_min)))
+    col_max[k], row_max[k] = col_max[k] + 1, row_max[k] + 1
+    max_result = max(max_result, col_cnt * (col_max[k] - col_min[k]), row_cnt * (row_max[k] - row_min[k]))
+print(max_result)
