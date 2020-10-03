37f44cfd487da82a20ffbe515db6bd80d25586e2
Lee-sungheon <leesungheon.dev@gmail.com>
2020-10-03 20:48:05 +0900

    201003 practice coding test

--- coding_test/beakjoon_2571.py
+++ coding_test/beakjoon_2571.py
@@ -1,90 +1,54 @@
 # 색종이 - 3
-def find_index(i, j):
-    col_min.append(j)
-    row_min.append(i)
-    for m in range(i, 101):
-        if matrix[m][j] > 1 and (m == 100 or matrix[m+1][j] <= 1):
-            row_max.append(m)
+def find_area(col_min, row_min):
+    max_result = 0
+    row_max = col_max = 0
+    is_true = False
+    for m in range(col_min, 101):
+        if matrix[m][row_min] == 1 and (m == 100 or matrix[m+1][row_min] == 0):
+            col_max = m
+            for i in range(row_min, 101):
+                for j in range(col_min, col_max+1):
+                    if matrix[j][i] == 0:
+                        row_max = j
+                        is_true = True
+                        break
+                if is_true:
+                    is_true = False
+                    break
             break
-    for n in range(j, 101):
-        if matrix[i][n] > 1 and (n == 100 or matrix[i][n+1] <= 1):
-            col_max.append(n)
+    max_result = max(max_result, ((row_max+1)-row_min)*((col_max+1)-col_min))
+    # print(row_min, row_max, col_min, col_max)
+    for n in range(row_min, 101):
+        if matrix[col_min][n] == 1 and (n == 100 or matrix[col_min][n+1] == 0):
+            row_max = n
+            for i in range(col_min, 101):
+                for j in range(row_min, row_max + 1):
+                    if matrix[i][j] == 0:
+                        col_max = i
+                        is_true = True
+                        break
+                if is_true:
+                    is_true = False
+                    break
             break
+    max_result = max(max_result, ((row_max+1) - row_min)*((col_max) - col_min))
+    # print(row_min, row_max, col_min, col_max)
+    return max_result
 
-    for i in range(row_min[-1], row_max[-1] + 1):
-        for j in range(col_min[-1], col_max[-1] + 1):
-            matrix[i][j] = 110
-    return
 
 
 N = int(input())
 matrix = [[0]*101 for _ in range(101)]
 for _ in range(N):
     x, y = map(int, input().split())
-    for i in range(y, y+10):
+    for i in range(y, y + 10):
         for j in range(x, x + 10):
-            matrix[i][j] += 1
-
-col_max, row_max = [], []
-col_min, row_min = [], []
-for i in range(101):
-    for j in range(101):
-        if 101 > matrix[i][j] > 1:
-            find_index(i, j)
+            matrix[i][j] = 1
 
 max_result = 0
-for k in range(len(row_min)):
-    if col_min[k] == 101:
-        col_min[k] = 0
-    if row_min[k] == 101:
-        row_min[k] = 0
-    col_cnt = row_max[k] - row_min[k]
-    for i in range(row_min[k] - 1, -1, -1):
-        tmp = 0
-        for j in range(col_min[k], col_max[k] + 1):
-            if matrix[i][j] >= 1:
-                tmp += 1
-            else:
-                break
-        if tmp == col_max[k] - col_min[k] + 1:
-            col_cnt += 1
-        else:
-            break
-    for i in range(row_max[k], 101):
-        tmp = 0
-        for j in range(col_min[k], col_max[k] + 1):
-            if matrix[i][j] >= 1:
-                tmp += 1
-            else:
-                break
-        if tmp == col_max[k] - col_min[k] + 1:
-            col_cnt += 1
-        else:
-            break
+for i in range(101):
+    for j in range(101):
+        if matrix[i][j] == 1:
+            max_result = max(max_result, find_area(i, j))
 
-    row_cnt = col_max[k] - col_min[k]
-    for i in range(col_min[k] - 1, -1, -1):
-        tmp = 0
-        for j in range(row_min[k], row_max[k] + 1):
-            if matrix[j][i] >= 1:
-                tmp += 1
-            else:
-                break
-        if tmp == row_max[k] - row_min[k] + 1:
-            row_cnt += 1
-        else:
-            break
-    for i in range(col_max[k], 101):
-        tmp = 0
-        for j in range(row_min[k], row_max[k] + 1):
-            if matrix[j][i] >= 1:
-                tmp += 1
-            else:
-                break
-        if tmp == row_max[k] - row_min[k] + 1:
-            row_cnt += 1
-        else:
-            break
-    col_max[k], row_max[k] = col_max[k] + 1, row_max[k] + 1
-    max_result = max(max_result, col_cnt * (col_max[k] - col_min[k]), row_cnt * (row_max[k] - row_min[k]))
 print(max_result)
