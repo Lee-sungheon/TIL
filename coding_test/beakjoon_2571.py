693b4a7c793980ed2682621e293741730a5dc402
Lee-sungheon <leesungheon.dev@gmail.com>
2020-10-06 09:20:36 +0900

    201006 TIL

--- coding_test/beakjoon_2571.py
+++ coding_test/beakjoon_2571.py
@@ -1,40 +1,22 @@
 # 색종이 - 3
-def find_area(col_min, row_min):
-    max_result = 0
-    row_max = col_max = 0
-    is_true = False
-    for m in range(col_min, 101):
-        if matrix[m][row_min] == 1 and (m == 100 or matrix[m+1][row_min] == 0):
-            col_max = m
-            for i in range(row_min, 101):
-                for j in range(col_min, col_max+1):
-                    if matrix[j][i] == 0:
-                        row_max = j
-                        is_true = True
-                        break
-                if is_true:
-                    is_true = False
-                    break
-            break
-    max_result = max(max_result, ((row_max+1)-row_min)*((col_max+1)-col_min))
-    # print(row_min, row_max, col_min, col_max)
-    for n in range(row_min, 101):
-        if matrix[col_min][n] == 1 and (n == 100 or matrix[col_min][n+1] == 0):
-            row_max = n
-            for i in range(col_min, 101):
-                for j in range(row_min, row_max + 1):
-                    if matrix[i][j] == 0:
-                        col_max = i
-                        is_true = True
-                        break
-                if is_true:
-                    is_true = False
-                    break
-            break
-    max_result = max(max_result, ((row_max+1) - row_min)*((col_max) - col_min))
-    # print(row_min, row_max, col_min, col_max)
-    return max_result
+def isPaper(r, c, w, h):
+    for i in range(r, r+h+1):
+        for j in range(c, c+w+1):
+            if matrix[i][j] != 1:
+                return 0
+    return 1
 
+
+def find_area(r, c):
+    global max_result
+    for w in range(101):
+        if c+w > 101 or matrix[r][c+w] == 0:
+            break
+        for h in range(101):
+            if r+h > 101 or matrix[r+h][c] == 0:
+                break
+            if isPaper(r, c, w, h):
+                max_result = max(max_result, (w+1)*(h+1))
 
 
 N = int(input())
@@ -44,11 +26,9 @@ for _ in range(N):
     for i in range(y, y + 10):
         for j in range(x, x + 10):
             matrix[i][j] = 1
-
 max_result = 0
-for i in range(101):
-    for j in range(101):
-        if matrix[i][j] == 1:
-            max_result = max(max_result, find_area(i, j))
-
+for r in range(101):
+    for c in range(101):
+        if matrix[r][c] == 1:
+            find_area(r, c)
 print(max_result)
