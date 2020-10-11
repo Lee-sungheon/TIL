c36acc5a3b63f4df80f992afacd400f70765e5bf
Lee-sungheon <leesungheon.dev@gmail.com>
2020-10-11 14:35:33 +0900

    201011 practice coding test

--- coding_test/beakjoon_3954.py
+++ coding_test/beakjoon_3954.py
@@ -1,44 +1,49 @@
 # Brainfuck 인터프리터
 
 T = int(input())
-for tc in range(T):
+for _ in range(T):
     m, c, i = map(int, input().split())
     brain = list(input())
-    pro_input = list(input())
-    pointer = 0
-    brain_li = [0] * c
-    idx = idx2 = cnt = 0
+    pro_input = list(map(ord, input())) + [255]
+    pair = {}
+    tmp_stack = []
+    for j in range(c):
+        if brain[j] == '[':
+            tmp_stack.append(j)
+        elif brain[j] == ']':
+            x = tmp_stack.pop()
+            pair[x] = j
+            pair[j] = x
+    brain_li = [0] * m
+    pointer = idx = idx2 = cnt = 0
     stack = []
-    stack2 = []
     while idx < c:
         if brain[idx] == '+':
-            brain_li[pointer] += 1
+            brain_li[pointer] = (brain_li[pointer] + 1) % 256
         elif brain[idx] == '-':
-            brain_li[pointer] -= 1
+            brain_li[pointer] = (brain_li[pointer] - 1) % 256
         elif brain[idx] == '<':
-            pointer -= 1
+            pointer = (pointer - 1) % m
         elif brain[idx] == '>':
-            pointer += 1
+            pointer = (pointer + 1) % m
         elif brain[idx] == ',':
+            brain_li[pointer] = pro_input[idx2]
             if idx2 < i:
-                brain_li[pointer] = ord(pro_input[idx2])
                 idx2 += 1
-            else:
-                brain_li[pointer] = 255
         elif brain[idx] == '[':
-            stack.append(idx)
             if brain_li[pointer] == 0:
-                idx = stack2.pop() - 1
+                idx = pair[idx]
+            else:
+                stack.append(idx)
         elif brain[idx] == ']':
             if brain_li[pointer] != 0:
-                stack2.append(idx)
-                idx = stack.pop() - 1
+                idx = pair[idx]
             else:
                 stack.pop()
         idx += 1
         cnt += 1
-        if cnt > 50000:
-            print("Loops")
+        if cnt >= 44000000:  # 50000000
+            print(f"Loops {stack[0]} {pair[stack[0]]}")
             break
     else:
         print('Terminates')
