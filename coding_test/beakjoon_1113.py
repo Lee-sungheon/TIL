5bd2ff1efe51f51fb631a14d6167b649e31b9fa7
Lee-sungheon <leesungheon.dev@gmail.com>
2020-10-18 00:30:56 +0900

    201018 practice coding test

--- coding_test/beakjoon_1113.py
+++ coding_test/beakjoon_1113.py
@@ -1,4 +1,9 @@
 # 수영장 만들기
+def is_range(r, c):
+    if r < 0 or c < 0 or r >= N or c >= M:
+        return 0
+    return 1
+
 
 N, M = map(int, input().split())
 pool = list(map(int, input().split()) for _ in range(N))
