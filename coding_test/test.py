9389be4ed43d4a238427e3f6705f8bf134d6046b
Lee-sungheon <leesungheon.dev@gmail.com>
2020-10-19 18:34:55 +0900

    201019 practice coding test

--- coding_test/test.py
+++ coding_test/test.py
@@ -1,55 +1,36 @@
-def check(x, y, dep):
-    global cnt
-    if dep >= cnt:
-        return
-    if matrix[y][x] == 1:
-        for k in range(5, 0, -1):
-            if 0 < x+k <= 10 and 0 < y+k <= 10 and recheck(x, y, k) and num[k] > 0:
-                change(x, y, k, 0)
-                num[k] -= 1
-                if x < 9:
-                    check(x+1, y, dep+1)
-                else:
-                    if y < 9:
-                        check((x+1) % 10, y+1, dep+1)
-                    else:
-                        check(x, y, dep+1)
-                change(x, y, k, 1)
-                num[k] += 1
-    else:
-        if x < 9:
-            check(x+1, y, dep)
-        else:
-            if y < 9:
-                check((x+1) % 10, y+1, dep)
-            else:
-                for n in range(10):
-                    for m in range(10):
-                        if matrix[n][m] == 1:
-                            return
-                else:
-                    if cnt > dep:
-                        cnt = dep
-
-def recheck(x, y, k):
-    for i in range(k):
-        for j in range(k):
-            if matrix[y+i][x+j] != 1:
-                return 0
-    return 1
+# 음식배달
+def search(r, c, cost):
+    tmp = []
+    for l in range(N):
+        for m in range(N):
+            if array[l][m] == 1:
+                tmp.append((cost+abs(r-l)+abs(c-m), cost))
+    total.append(tmp)
 
 
-def change(x, y, k, value):
-    for i in range(y, y+k):
-        for j in range(x, x+k):
-            matrix[i][j] = value
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    array = [list(map(int, input().split())) for _ in range(N)]
+    food = set()
+    total = []
+    result = 0
 
+    for i in range(N):
+        for j in range(N):
+            if array[i][j] > 1:
+                search(i, j, array[i][j])
+    print(total)
 
-matrix = [list(map(int, input().split())) for _ in range(10)]
-num = [0, 5, 5, 5, 5, 5]
-cnt = 26
-check(0, 0, 0)
-if cnt == 26:
-    print(-1)
-else:
-    print(cnt)
+    for i in range(len(total[0])):
+        min_num = [1000000000, 0]
+        for j in range(len(total)):
+            if total[j][i][0] < min_num[0]:
+                min_num[0] = total[j][i][0]
+                min_num[1] = total[j][i][1]
+        print(min_num)
+        result += min_num[0] - min_num[1]
+        food.add(min_num[1])
+    result += sum(food)
+    print(food)
+    print(result)
