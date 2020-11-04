864bc040c81d61264053be393834f6799df50582
Lee-sungheon <leesungheon.dev@gmail.com>
2020-11-04 21:11:07 +0900

    201104 practice algorithm

--- coding_test/beakjoon_3110.py
+++ coding_test/beakjoon_3110.py
@@ -1,11 +1,4 @@
 # 부등식
-def change(x):
-    if x > int(x):
-        return int(x)
-    else:
-        return int(x) - 1
-
-
 B2, C2, D2 = map(int, input().split())
 A1, A2 = map(int, input().split())
 E1, E2 = map(int, input().split())
@@ -14,16 +7,6 @@ C0 = int(A1/A2*C2) - 1
 C1 = int(C2*E1/E2) + 1
 
 for i in range(C0, C1):
-    A = A1/A2*B2
-    Cs = i/C2*B2
-    A = change(A)
-    Cs = change(Cs)
-    E = E1/E2*D2
-    Ce = i/C2*D2
-    Ce = change(Ce)
-    E = change(E)
     if A1/A2 < i/C2 < E1/E2:
-        result += (Cs-A)*(E-Ce)
+        result += ((B2*i-1)//C2-(B2*A1//A2))*((D2*E1-1)//E2-(D2*i//C2))
 print(result)
-
-
