955c37cc2d0a7b5fb4e9e334c4fd93d7687b1dd7
Lee-sungheon <leesungheon.dev@gmail.com>
2020-12-22 17:52:54 +0900

    201222 TIL

--- coding_test/test.py
+++ coding_test/test.py
@@ -1,42 +1,77 @@
-from collections import defaultdict, deque
 import sys
 
-
-def bfs(D):
-    global max_cnt
-    res = 0
-    S = deque([D])
-    visit[D] = False
-    while S:
-        res += 1
-        tmp = S.popleft()
-        for i in dic[tmp]:
-            if visit[i]:
-                S.append(i)
-                visit[i] = False
-    if max_cnt < res:
-        L.clear()
-        L.append(D)
-        max_cnt = res
-    elif max_cnt == res:
-        L.append(D)
-    return
-
-
 input = sys.stdin.readline
-
-N, M = map(int, input().split())
-L = []
-max_cnt = 0
-dic = [[] for _ in range(N+1)]
-for _ in range(M):
+sys.setrecursionlimit(10 ** 9)
+n, m = map(int, input().split())
+adj = [[] for _ in range(n)]
+inv = [[] for _ in range(n)]
+for _ in range(m):
     a, b = map(int, input().split())
-    dic[b].append(a)
+    adj[b - 1].append(a - 1)
+    inv[a - 1].append(b - 1)
+stack = []
+v = [0] * n
 
-for key in range(len(dic)):
-    if dic[key]:
-        visit = [True] * (N+1)
-        bfs(key)
 
-for i in L:
-    print(i, end=' ')
+def dfs(i):
+    v[i] = 1
+    for j in adj[i]:
+        if v[j] == 0:
+            dfs(j)
+    stack.append(i)
+
+
+for i in range(n):
+    if v[i] == 0:
+        dfs(i)
+v = [0] * n
+scc = [0] * n
+scc_num = 0
+scc_size = []
+
+
+def dfs_inv(i):
+    global size
+    v[i] = 1
+    scc[i] = scc_num
+    size += 1
+    for j in inv[i]:
+        if v[j] == 0:
+            dfs_inv(j)
+
+
+while stack:
+    i = stack.pop()
+    if v[i] == 0:
+        size = 0
+        dfs_inv(i)
+        scc_size.append(size)
+        scc_num += 1
+scc_adj = [set() for _ in range(scc_num)]
+scc_inv = [set() for _ in range(scc_num)]
+for i in range(n):
+    for j in adj[i]:
+        if scc[i] != scc[j]:
+            scc_adj[scc[i]].add(scc[j])
+            scc_inv[scc[j]].add(scc[i])
+scc_outd = [len(scc_adj[i]) for i in range(scc_num)]
+d = [set() for _ in range(scc_num)]
+Q = []
+for i in range(scc_num):
+    if scc_outd[i] == 0:
+        Q.append(i)
+while Q:
+    q = []
+    for i in Q:
+        d[i].add(i)
+        for j in scc_inv[i]:
+            d[j] |= d[i]
+            scc_outd[j] -= 1
+            if scc_outd[j] == 0:
+                q.append(j)
+    Q = q
+ds = [sum(scc_size[j] for j in d[i]) for i in range(scc_num)]
+M = max(ds)
+for i in range(n):
+    if ds[scc[i]] == M:
+        print(i + 1, end=' ')
