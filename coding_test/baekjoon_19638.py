0a01b255f65c584c8d78ec4068293be057cd9479
Lee-sungheon <leesungheon.dev@gmail.com>
2021-01-09 18:07:33 +0900

    210109 practice algorithm

--- coding_test/baekjoon_19638.py
+++ coding_test/baekjoon_19638.py
@@ -1,5 +1,21 @@
 # 센티와 마법의 뿅망치
+import heapq, sys
 
+input = sys.stdin.readline
 N, H, T = map(int, input().split())
 giant = []
-for i in range(N):
+for _ in range(N):
+    heapq.heappush(giant, -int(input()))
+for i in range(T):
+    if -giant[0] < H:
+        print("YES\n{}" .format(i))
+        exit(0)
+    if -giant[0] == 1:
+        break
+    tmp = -heapq.heappop(giant)
+    tmp = tmp//2
+    heapq.heappush(giant, -tmp)
+if -giant[0] < H:
+    print("YES\n{}" .format(T))
+    exit(0)
+print("NO\n{}" .format(-giant[0]))
