a323cdf9f5c13f7c206c7f8043570bd6d3dd0163
Lee-sungheon <leesungheon.dev@gmail.com>
2021-01-20 15:09:23 +0900

    210120 practice algorithm

---
+++ coding_test/swea_3752.py
@@ -0,0 +1,13 @@
+# 가능한 시험 점수
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    scores = list(map(int, input().split()))
+    result = set()
+    result.add(0)
+    tmp = set()
+    for i in range(len(scores)):
+        for j in result:
+            tmp.add(j + scores[i])
+        result.update(tmp)
+    print(f'#{tc+1} {len(result)}')
