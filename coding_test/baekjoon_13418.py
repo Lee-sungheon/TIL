10a5de70abb85b9377f27606e12c216f21d0b444
Lee-sungheon <leesungheon.dev@gmail.com>
2021-06-22 18:29:33 +0900

    210622 pratice algorithm

--- coding_test/baekjoon_13418.py
+++ coding_test/baekjoon_13418.py
@@ -7,15 +7,15 @@ input = sys.stdin.readline
 
 
 def prim(start):
-    tree_set = {start}
     q = []
     result = 0
+    visited[start] = 1
     for edge in graph[start]:
         heappush(q, edge)
     while q:
         weight, x = heappop(q)
-        if x not in tree_set:
-            tree_set.add(x)
+        if visited[x] == 0:
+            visited[x] = 1
             result += weight
             for edge in graph[x]:
                 heappush(q, edge)
@@ -24,19 +24,15 @@ def prim(start):
 
 v, e = map(int, input().split())
 graph = [[] for _ in range(v+1)]
-value1 = 0
+visited = [0] * (v+1)
 for _ in range(e):
     a, b, c = map(int, input().split())
-    if c == 0:  c = 1
-    else:  c = 0
-    graph[a].append([c, b])
-    graph[b].append([c, a])
+    graph[a].append([1-c, b])
+    graph[b].append([1-c, a])
 value1 = prim(0)**2
 for i in range(len(graph)):
     for j in range(len(graph[i])):
-        if graph[i][j][0] == 1:
-            graph[i][j][0] = 0
-        else:
-            graph[i][j][0] = -1
+        graph[i][j][0] -= 1
+visited = [0] * (v+1)
 value2 = prim(0)**2
 print(value2 - value1)
