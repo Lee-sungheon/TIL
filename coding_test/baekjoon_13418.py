fd2a600fdaeec8daf9b3358f9cde659b41eb5cc3
Lee-sungheon <leesungheon.dev@gmail.com>
2021-06-23 18:26:57 +0900

    210623 pratice algorithm

--- coding_test/baekjoon_13418.py
+++ coding_test/baekjoon_13418.py
@@ -6,33 +6,27 @@ from heapq import *
 input = sys.stdin.readline
 
 
-def prim(start):
-    q = []
+def prim(start, plus):
+    visited = [False] * (v+1)
     result = 0
     visited[start] = 1
-    for edge in graph[start]:
-        heappush(q, edge)
+    q = [(edge[0]*plus, edge[1]) for edge in graph[start]]
+    heapify(q)
     while q:
         weight, x = heappop(q)
-        if visited[x] == 0:
-            visited[x] = 1
+        if not visited[x]:
+            visited[x] = True
             result += weight
             for edge in graph[x]:
-                heappush(q, edge)
+                if not visited[edge[1]]:
+                    heappush(q, (edge[0]*plus, edge[1]))
     return result
 
 
 v, e = map(int, input().split())
 graph = [[] for _ in range(v+1)]
-visited = [0] * (v+1)
-for _ in range(e):
+for _ in range(e+1):
     a, b, c = map(int, input().split())
-    graph[a].append([1-c, b])
-    graph[b].append([1-c, a])
-value1 = prim(0)**2
-for i in range(len(graph)):
-    for j in range(len(graph[i])):
-        graph[i][j][0] -= 1
-visited = [0] * (v+1)
-value2 = prim(0)**2
-print(value2 - value1)
+    graph[a].append((1^c, b))
+    graph[b].append((1^c, a))
+print(prim(0, -1)**2 - prim(0, 1)**2)
