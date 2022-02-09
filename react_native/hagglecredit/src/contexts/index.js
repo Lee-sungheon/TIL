03b9a278fea313a6b30368d9120c204e76b8449a
이성헌 <rkd028@naver.com>
2022-02-09 22:29:31 +0900

    Delete react_native directory

--- react_native/hagglecredit/src/contexts/index.js
+++
@@ -1,5 +0,0 @@
-import { UserContext, UserProvider } from "./User";
-import { ProgressContext, ProgressProvider } from "./Progress";
-
-export { UserContext, UserProvider };
-export { ProgressContext, ProgressProvider };
