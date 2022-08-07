d6e58e5438d580ad86380a6d02eca48f0a50bee6
Lee-sungheon <leesungheon.dev@gmail.com>
2022-08-07 15:14:06 +0900

    필요없는 파일들 정리

--- react_native/hagglecredit/src/components/common/index.js
+++
@@ -1,6 +0,0 @@
-import Button from "./Button";
-import Image from "./Image";
-import Input from "./Input";
-import Spinner from "./Spinner";
-import ErrorMessage from "./ErrorMessage";
-export { Button, Image, Input, ErrorMessage, Spinner };
